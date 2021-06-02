7b133336d17097d59897f094c6fb9e6047b9392a
Abdynasyr <abdynasyr@gmail.com>
2021-06-02 23:29:37 +0600

    first commit

--- blog/settings.py
+++ blog/settings.py
@@ -18,7 +18,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '6uf@xbvv-7u+f(6(ihu4(i#bcn@5)^n029m2sg32n8nbzk#(fe'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -80,8 +80,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'blog',
-        'USER': 'abdynasyr',
-        'PASSWORD': '1',
+        'USER': '',
+        'PASSWORD': '',
         'HOST': 'localhost',
         'PORT': 5432
     }
